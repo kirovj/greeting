d40cdff3b013147979311088dada342726dcad2c
J-wyt <546108612@qq.com>
2021-09-10 22:49:31 +0800

    如果变量被声明为可变的，则函数参数使用可变引用也可以工作

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,16 @@ struct Number {
     value: i32,
 }
 
-// the `Neg` trait is used to overload `-`, the
-// unary minus operator.
-impl std::ops::Neg for Number {
-    type Output = Self;
-    fn neg(self) -> Self {
-        Self {
-            value: -self.value,
-            odd: self.odd,
-        }        
-    }
-}
-
-fn print_i32(x: i32) {
-    println!("x = {}", x);
+fn invert(n: &mut Number) {
+    n.value = -n.value;
 }
 fn print_number(n: &Number) {
     println!("{} number {}", if n.odd { "odd" } else { "even" }, n.value);
 }
 fn main() {
-    let n = Number { odd: true, value: 51 };
-    print_number(&n); // `n` is borrowed for the time of the call
-    print_number(&n); // `n` is borrowed again
+    // this time, `n` is mutable
+    let mut n = Number { odd: true, value: 51 };
+    print_number(&n);
+    invert(&mut n); // `n is borrowed mutably - everything is explicit
+    print_number(&n);
 }

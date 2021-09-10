b16bb28a4f69e7efe1135cf964748ba1d11466bb
J-wyt <546108612@qq.com>
2021-09-10 22:56:56 +0800

    泛型函数可以被当作一个命名空间，包含无穷多个不同具体类型的函数。 类似crate、module和类型，泛型函数可以使用::导航

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ where
     println!("{:?} {} {:?}", left, if left == right { "==" } else { "!=" }, right);
 }
 fn main() {
-    compare("tea", "coffee");
-    // prints: "tea" != "coffee"
+    use std::any::type_name;
+    println!("{}", type_name::<i32>()); // prints "i32"
+    println!("{}", type_name::<(f64, char)>()); // prints "(f64, char)"
 }

bda79ba4da18346b8ed149b0d88e638058109e67
J-wyt <546108612@qq.com>
2021-09-11 01:58:49 +0800

    我们使用 enum 关键字创建枚举类型，该类型可以具有枚举变体的任意组合。 与结构一样，枚举变体可以具有命名字段、没有名称的字段或根本没有字段。 与结构类型一样，枚举类型也采用大写形式

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+enum WebEvent {
+    // An enum variant can be like a unit struct without fields or data types
+    WELoad,
+    // An enum variant can be like a tuple struct with data types but no named fields
+    WEKeys(String, char),
+    // An enum variant can be like a classic struct with named fields and their data types
+    WEClick { x: i64, y: i64 },
+}
+
 fn main() {
     let formal = true;
     let greeting = if formal {

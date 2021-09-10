21e15e63c0a8f20dd3f10199c980cf8da7376d39
J-wyt <546108612@qq.com>
2021-09-10 23:25:31 +0800

    panic也是一个宏，例如Option既可以包含某个值，也可以不包含值。如果它不包含值，调用它的.unwrap()会panic

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ fn print_type_name<T>(_val: &T) {
     println!("{}", std::any::type_name::<T>());
 }
 fn main() {
-    println!("{}", "Hello there!");
+    let o1: Option<i32> = Some(128);
+    o1.unwrap(); // this is fine
+    let o2: Option<i32> = None;
+    o2.unwrap(); // this panics!
 }
-// fn main() {
-//     use std::io::{self, Write};
-//     io::stdout().lock().write_all(b"Hello there!\n").unwrap();
-// }
+// output: thread 'main' panicked at 'called `Option::unwrap()` on a `None` value', src/libcore/option.rs:378:21

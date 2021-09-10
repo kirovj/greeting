d3d12fa1d0bc00cc0fda687c06579e15fdd682c2
J-wyt <546108612@qq.com>
2021-09-11 03:27:04 +0800

    创建迭代器的另一种简单方法是使用范围表示法 a..b

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
-    let big_birds = ["ostrich", "peacock", "stork"];
-    for bird in big_birds.iter() {
-        println!("The {} is a big bird.", bird);
+    for number in 0..5 {
+        println!("{}", number * 2);
     }
 }

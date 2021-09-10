041a1c69819ba9b3f44f2888e902abe26630cb9c
J-wyt <546108612@qq.com>
2021-09-11 02:03:01 +0800

    实例化枚举 接下来添加代码以创建枚举变体的实例。 对于每个变体，我们使用 let 关键字进行赋值。 为了访问枚举定义中的特定变体，我们使用带有双冒号 :: 的语法 <enum>::<variant>。

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
-enum WebEvent {
-    // An enum variant can be like a unit struct without fields or data types
-    WELoad,
-    // An enum variant can be like a tuple struct with data types but no named fields
-    WEKeys(String, char),
-    // An enum variant can be like a classic struct with named fields and their data types
-    WEClick { x: i64, y: i64 },
+// Define a tuple struct
+struct KeyPress(String, char);
+
+// Define a classic struct
+struct MouseClick {
+    x: i64,
+    y: i64,
 }
 
-// WELoad 没有关联的数据类型或数据。
-// WEKeys 具有两个数据类型分别为 String 和 char 的字段。
-// WEMClick 包含命名字段为 x 和 y 以及字段的数据类型为 i64 的匿名结构。
-
+// Redefine the enum variants to use the data from the new structs
+// Update the page Load variant to have the boolean type
+enum WebEvent {
+    WELoad(bool),
+    WEClick(MouseClick),
+    WEKeys(KeyPress),
+}
 fn main() {
-    let formal = true;
-    let greeting = if formal {
-        // "if" keyword used here as an expression
-        "Good day to you." // Returns the string "Good day to you."
-    } else {
-        "Hey!" // Returns the string "Hey!"
-    };
-    println!("{}", greeting) // Prints "Good day to you."
+    let we_load = WebEvent::WELoad(true);
 }

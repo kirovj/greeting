70f9ed72dec7dcdfef5b0c26418bfb1563acf074
J-wyt <546108612@qq.com>
2021-09-10 22:47:58 +0800

    但是Number类型没有实现Copy,所以下面的代码不工作

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn print_i32(x: i32) {
     println!("x = {}", x);
 }
 fn main() {
-    let a: i32 = 15;
-    print_i32(a); // `a` is copied
-    print_i32(a); // `a` is copied again
+    let n = Number { odd: true, value: 51 };
+    let m = n; // `n` is moved into `m`
+    let o = n; // error: use of moved value: `n`
 }

47dcf0df8cde1e3f516d01f187f417252e6ddef0
J-wyt <546108612@qq.com>
2021-09-10 23:24:57 +0800

    类似name!()、name![]、name!{}都是调用宏的方式。宏会被展开成正常的代码。 事实上，println就是一个宏,它的展开代码和下面的代码功能一样

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ fn print_type_name<T>(_val: &T) {
     println!("{}", std::any::type_name::<T>());
 }
 fn main() {
-    let v1 = vec![1, 2, 3];
-    let v2 = vec![true, false, true];
-    print_type_name(&v1); // prints "Vec<i32>"
-    print_type_name(&v2); // prints "Vec<bool>"
+    println!("{}", "Hello there!");
 }
+// fn main() {
+//     use std::io::{self, Write};
+//     io::stdout().lock().write_all(b"Hello there!\n").unwrap();
+// }

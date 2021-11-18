3938c3532a1a7f27b470ef0920945ade69c2d9a8
J-wyt <546108612@qq.com>
2021-11-19 00:14:46 +0800

    vec! 宏 新建一个包含初值的 vector

--- list/src/main.rs
+++ list/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     // let v: Vec<i32> = Vec::new();
-    let v = vec![1, 2, 3]; // vec! 宏 新建一个包含初值的 vector
+    let mut v = vec![1, 2, 3]; // vec! 宏 新建一个包含初值的 vector
 
     v.push(5);
 }

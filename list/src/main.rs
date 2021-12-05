02559f3232bcbf59858d74088db8d0ad6f51c7b3
J-wyt <546108612@qq.com>
2021-12-05 23:19:19 +0800

    一旦键值对被插入后就为哈希 map 所拥有,除了实现了 Copy trait 的类型

--- list/src/main.rs
+++ list/src/main.rs
@@ -3,9 +3,11 @@
 use std::collections::HashMap;
 
 fn main() {
-    let teams = vec![String::from("Bule"), String::from("Yellow")];
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
 
-    let init_scores = vec![10, 50];
-
-    let score: HashMap<_, _> = teams.iter().zip(init_scores.iter()).collect();
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // 这里 field_name 和 field_value 不再有效，
+    // 尝试使用它们看看会出现什么编译错误！
 }

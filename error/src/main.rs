1823a31485593e3d9d8b8889f61a9d4667a83250
J-wyt <546108612@qq.com>
2021-12-07 01:56:54 +0800

    ? link

--- error/src/main.rs
+++ error/src/main.rs
@@ -4,9 +4,8 @@ use std::{
 };
 
 fn read_file_as_name() -> Result<String, io::Error> {
-    let mut f = File::open("hello.txt")?;
     let mut s = String::new();
-    f.read_to_string(&mut s)?;
+    File::open("hello.txt")?.read_to_string(&mut s)?;
     Ok(s)
 }
 

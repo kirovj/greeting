940f80a18acef4c0f5ecc2fe93de9379510c64c2
J-wyt <546108612@qq.com>
2021-12-07 01:41:20 +0800

    fn return Result

--- error/src/main.rs
+++ error/src/main.rs
@@ -1,5 +1,25 @@
-use std::fs::File;
+use std::{
+    fs::File,
+    io::{self, Read},
+};
+
+fn read_file_as_name() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
 
 fn main() {
-    let f = File::open("hello.txt").expect("msg");
+    let name = read_file_as_name().unwrap();
+    print!("name: {}", &name);
 }

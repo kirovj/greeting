c84dad7398fc4e1aefcac34c19bd506d7b66694c
J-wyt <546108612@qq.com>
2021-12-08 02:04:02 +0800

    complete find_largest

--- generic/src/lib.rs
+++ generic/src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 use std::fmt::Display;
 
 pub trait Summary {
--- generic/src/main.rs
+++ generic/src/main.rs
@@ -1,28 +1,23 @@
+#![allow(unused)]
 use generic::{notify, Summary, Tweet};
 
-// fn find_largest(list: &[i32]) -> i32 {
-//     let mut largest = list[0];
-//     for &item in list {
-//         if item > largest {
-//             largest = item
-//         }
-//     }
-//     largest
-// }
+fn find_largest<T: PartialOrd + Clone>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+    for item in list.iter() {
+        if item > largest {
+            largest = item
+        }
+    }
+    largest
+}
 
 fn main() {
-    let tweet = Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        retweet: false,
-    };
+    let list = vec![10, 34, 23];
+    println!("largest: {}", find_largest(&list));
 
-    println!(
-        "1 new tweet: {}\nfrom: {}",
-        tweet.summarize(),
-        tweet.summarize_author()
-    );
+    let list = vec!['c', 'b', 'e'];
+    println!("largest: {}", find_largest(&list));
 
-    notify(tweet)
+    let list = vec![String::from("hello"), String::from("world")];
+    println!("largest: {}", find_largest(&list));
 }

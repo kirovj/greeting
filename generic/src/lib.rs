41947a60efe8bb68514d89637453894a5ad61dd1
J-wyt <546108612@qq.com>
2021-12-08 01:39:06 +0800

    trait & impl

---
+++ generic/src/lib.rs
@@ -0,0 +1,29 @@
+pub trait Summary {
+    fn sum(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn sum(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn sum(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
--- generic/src/main.rs
+++ generic/src/main.rs
@@ -1,14 +1,22 @@
-fn find_largest(list: &[i32]) -> i32 {
-    let mut largest = list[0];
-    for &item in list {
-        if item > largest {
-            largest = item
-        }
-    }
-    largest
-}
+use generic::{Summary, Tweet};
+
+// fn find_largest(list: &[i32]) -> i32 {
+//     let mut largest = list[0];
+//     for &item in list {
+//         if item > largest {
+//             largest = item
+//         }
+//     }
+//     largest
+// }
 
 fn main() {
-    let list = vec![10, 34, 23];
-    print!("largest: {}", find_largest(&list))
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.sum());
 }

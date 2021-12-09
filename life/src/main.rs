52e7462d18a429fbce7385223816f31ab288c347
J-wyt <546108612@qq.com>
2021-12-10 00:52:05 +0800

    lifetime in struct

--- life/src/main.rs
+++ life/src/main.rs
@@ -1,12 +1,11 @@
-fn main() {
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
-
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {}", result);
+struct People<'a> {
+    name: &'a str,
 }
 
-fn longest<'a>(x: &'a str, y: &'a str) -> String {
-    let result = String::from("asdasd");
-    result
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = People {
+        name: first_sentence,
+    };
 }

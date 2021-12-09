22fe6733ef0717e3231bb18224830fd3ba10029e
J-wyt <546108612@qq.com>
2021-12-10 01:09:08 +0800

    test init

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,8 +1,17 @@
+#![allow(unused)]
+
 #[cfg(test)]
 mod tests {
+
     #[test]
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn it_failed() {
+        let result = 2 + 2;
+        assert_eq!(result, 5);
     }
 }

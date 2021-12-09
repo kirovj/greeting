2f42dec110b34936435e8154627e4895fe18f133
J-wyt <546108612@qq.com>
2021-12-10 01:29:54 +0800

    should_panic(expected =

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -6,8 +6,10 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: u32) -> Guess {
-        if value < 1 || value > 200 {
-            panic!("value should between 1, 100")
+        if value < 1 {
+            panic!("value < 1")
+        } else if value > 100 {
+            panic!("value > 100")
         }
         Guess { value }
     }
@@ -18,7 +20,7 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "100")]
     fn guess_new() {
         Guess::new(200);
     }

4d48b23d025ea7d42a0a503c586a0a45cc9148f8
J-wyt <546108612@qq.com>
2021-12-10 01:17:58 +0800

    assert test

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,17 +1,34 @@
 #![allow(unused)]
 
+#[derive(Debug)]
+pub struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.height > other.height && self.width > other.width
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
 
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+    fn can_hold_test() {
+        let a = Rectangle {
+            width: 8,
+            height: 7,
+        };
 
-    #[test]
-    fn it_failed() {
-        let result = 2 + 2;
-        assert_eq!(result, 5);
+        let b = Rectangle {
+            width: 5,
+            height: 6,
+        };
+
+        assert!(a.can_hold(&b));
+        assert!(!b.can_hold(&a))
     }
 }

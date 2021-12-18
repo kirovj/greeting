9d6fe51e77c0a3664e2a3afe4d6079327514e3da
J-wyt <546108612@qq.com>
2021-12-19 03:12:38 +0800

    impl Deref

--- my_ref/src/main.rs
+++ my_ref/src/main.rs
@@ -1,10 +1,24 @@
-use crate::List::{Cons, Nil};
+use std::ops::Deref;
 
-fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
 }
 
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+fn main() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(x, *y)
+    // *y => *(y.deref())
 }

265036ed3e94f5a9912b9fa659526cfbd7d614e7
J-wyt <546108612@qq.com>
2021-12-27 00:52:26 +0800

    rc start

--- my_ref/src/main.rs
+++ my_ref/src/main.rs
@@ -1,59 +1,12 @@
-use std::ops::Deref;
-
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
 }
 
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        &self.0
-    }
-}
-
-fn hello(name: &str) {
-    println!("Hello, {}", name);
-}
-
-struct Person {
-    name: String,
-}
-
-impl Drop for Person {
-    fn drop(&mut self) {
-        println!("drop: {}", self.name);
-    }
-}
+use crate::List::{Cons, Nil};
 
 fn main() {
-    let x = 5;
-    let y = MyBox::new(x);
-
-    assert_eq!(x, *y);
-    // *y => *(y.deref())
-
-    let m = MyBox::new(String::from("jack"));
-    // &m &MyBox<String>
-    // deref &String
-    // deref &str
-    hello(&m);
-    // hello(&(*m)[..])
-    hello("rust");
-
-    let p = Person {
-        name: String::from("bob"),
-    };
-
-    let p1 = Person {
-        name: String::from("judy"),
-    };
-
-    println!("hello! {}", p.name);
-    drop(p);
-    println!("hello! {}", p1.name);
+    let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
+    let b = Cons(3, Box::new(a));
+    let c = Cons(4, Box::new(a));
 }
